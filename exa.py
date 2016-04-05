d693238042fc8002f712490859d8484ba9c15be4
prasenjitj <Prasenjit.jana@gmail.com>
2016-04-05 23:01:10 +0530

    Update exa.py

--- exa.py
+++ exa.py
@@ -3,22 +3,42 @@ import pprint
 import re
 from googleapiclient.discovery import build
 with open('input.csv', 'rb') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		#print (row['Title'], row['Url'], row['Director'], row['Alias'], row['Year'])
-		query = row['Title']+' imdb '+row['Director']
-		print 'Query :: ',query
-		keys = ['AIzaSyBlU4hsYnl6oSvrKw8ZKiTrRmZzBUBANWs','AIzaSyB7BqsM_vljNSPOq4twbB7N3sUutyjoobE','AIzaSyCv6pMQ4ObhFbtWuM5Ge5CEmLZ5o_VcuXQ']	
-		service = build("customsearch", "v1", developerKey="AIzaSyCv6pMQ4ObhFbtWuM5Ge5CEmLZ5o_VcuXQ")
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+    #print (row['Title'], row['Url'], row['Director'], row['Alias'], row['Year'])
+    print "===================================="
+    query = row['Title']+' imdb '+row['Director']
+    title = row['Title']
+    print 'Tite:',title
+    director = row['Director']
+    director = re.sub(r', ','|',director)
+    print 'Director:',director
+    print 'Query :: ',query
+    keys = ['AIzaSyBlU4hsYnl6oSvrKw8ZKiTrRmZzBUBANWs','AIzaSyB7BqsM_vljNSPOq4twbB7N3sUutyjoobE','AIzaSyCv6pMQ4ObhFbtWuM5Ge5CEmLZ5o_VcuXQ']	
+    service = build("customsearch", "v1", developerKey="AIzaSyB7BqsM_vljNSPOq4twbB7N3sUutyjoobE")
+    res = service.cse().list(q=query,cx='009241977094486741223:3agszpaq5sy',).execute()
+    #pprint.pprint(res)
+    #print type(res)
+    
+    try:
+      for i in range(0,len(res['items'])):
+        
+        print i
+        json_title = res['items'][i]['htmlTitle']
+        #print json_title.encode('utf8')
+        json_director =res['items'][i]['htmlSnippet']
+        #print json_director.encode('utf8')
 
-  		res = service.cse().list(
-      	q=query,cx='009241977094486741223:3agszpaq5sy',).execute()
-  		pprint.pprint(res)
-  		print "===================================="
-  		try:
-  			imdb_key = res['items'][0]['formattedUrl']
-  			imdb_key = re.search('^www.*?title/(.*?)/.*',imdb_key)
-  			print 'IMDB Key :: %s' % imdb_key.group(1)
-  		except Exception, e:
-  			print "Imdb key not found"
+        if re.search(title,json_title) and re.search(director,json_director):
+          imdb_key = res['items'][i]['formattedUrl']
+          imdb_key = re.search('^www.*?title/(.*?)/.*',imdb_key)
+          print 'IMDB Key :: ', imdb_key.group(1)
+          break
+        else:
+          print 'KEY NOT FOUND'       
+
+      
+    except Exception, e:
+      print "Exception",e
+
   		
