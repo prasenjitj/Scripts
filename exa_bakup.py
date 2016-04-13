6a7bcee22990759b8b6f700cafb5be7b1d466aa4
prasenjitj <Prasenjit.jana@gmail.com>
2016-04-13 16:01:30 +0530

    Added files via upload

--- exa_bakup.py
+++ exa_bakup.py
@@ -1,87 +1,131 @@
-import csv
-#import traceback
-#import pprint
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import re
+import csv
+import MySQLdb
 from googleapiclient.discovery import build
-ik =[]
-flag_1 = 0
-flag_2 = 0
+
+conn = MySQLdb.connect('localhost','root','123','test')
+print('Connected to MySQL database')
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM api")
+rows = cursor.fetchall()
+
 count = 0
 keycount = 0
 key = 0
-with open('input.csv', 'rb') as csvinput:
-	with open ('output.csv', 'wb') as csvoutput: 
+keycount = 0
+keys = 1
+key_id =''
+with open('Book1.csv', 'rb') as csvinput:
+	with open ('output3.csv', 'wb') as csvoutput: 
 		reader = csv.DictReader(csvinput)
-		fieldnames = ['Entity','url','Director','Year','Alias','IMDB URL','genre','Runtime']
+		fieldnames = ['Entity','url','Director','Year','Alias','IMDB_OLD','IMDB URL','genre','Runtime']
 		writer = csv.DictWriter(csvoutput, fieldnames=fieldnames)
 		writer.writeheader()
 
 		for row in reader:
-			#print (row['Title'], row['Url'], row['Director'], row['Alias'], row['Year'])
-			print "===================================="
+			# print (row['Entity'], row['Url'], row['Director'], row['Alias'], row['Year'])
 			
-			query = row['Entity']+' '+row['Director']+' '+row['Year']+' imdb'
-			title = row['Entity']
-			
-			director = row['Director']
-			director = re.sub(r', ','|',director)
+			t = row['Entity'].lower()
+			d = row['Director'].lower()
+			d = re.sub(r'(,\s.*)','',d).strip()
+			print t, d
 			year = row['Year']
 			alias = row['Alias']
 			url = row['url']
 			genre = row['genre']
 			runtime = row['Runtime']
-			print 'Tite:',title
-			print 'Director:',director
-			print 'Year:',year
-			print 'Alias:',alias
-			print 'URL:',url
-			print 'Genre:',genre
-			print 'Runtime:',runtime
-			print 'Query :: ',query
-			
-			Api_keys = ['AIzaSyBlU4hsYnl6oSvrKw8ZKiTrRmZzBUBANWs','AIzaSyB7BqsM_vljNSPOq4twbB7N3sUutyjoobE','AIzaSyCv6pMQ4ObhFbtWuM5Ge5CEmLZ5o_VcuXQ']
-			if count >= 81:
+			IMDB_OLD = row['IMDB_OLD']
+			query = t
+			# print 'Tite:',title
+			# print 'Director:',director
+			# print 'Year:',year
+			# print 'Alias:',alias
+			# print 'URL:',url
+			# print 'Genre:',genre
+			# print 'Runtime:',runtime
+			# print 'Query :: ',query
+			# query = 'Showboy'
+
+			keycount += 1
+			if keycount == 99:
 				keycount = 1
-				if keycount == 1:
-					service = build("customsearch", "v1", developerKey=Api_keys[key])
-					res = service.cse().list(q=query,cx='009241977094486741223:3agszpaq5sy',).execute()
-					response = res.get('items')
-					key =+ 1
-				count += 1
-			else:
-				count = 0
-			
-			#pprint.pprint(res)
-			# response = res.get('items')
+			if keycount == 1:
+				keys	+= 1
+				for i in rows:
+					key_id = i[1]
+					print key_id
+
+			service = build("customsearch", "v1", developerKey='AIzaSyBU49J2cMO9FDJBWwhNXP2OnsrFuOKU868')
+			res = service.cse().list(q=query,cx='009241977094486741223:3agszpaq5sy',).execute()
+			response = res.get('items')
+
+			# pprint.pprint(res)
+			response = res.get('items')
+
+			imdb_key =''
 			if response:
+				
+
+
 				for i in range(0,len(response)):
-
-					json_title = res['items'][i]['htmlTitle']
+					flag = 0
+					check = 0
+					print "===================================="
+					json_title = res['items'][i]['pagemap']['metatags'][0]['title'].lower()
 					json_title = re.sub(r'<.*?>','',json_title)
-					print 'fetched title:',json_title.encode('utf8')
-					
-					json_director =res['items'][i]['htmlSnippet']
-					json_director = re.sub(r'<.*?>','',json_director)
-					print 'fetched director:',json_director.encode('utf8')
-					
+					# print 'fetched title:',json_title
+					json_director = res['items'][i]['snippet'].lower()
+					# print json_director.encode('utf-8')
+					json_director = re.search(d,json_director,re.UNICODE)
 
-					if re.search(title,json_title,re.IGNORECASE) and re.search(director,json_director,re.IGNORECASE):
-						imdb_key = res['items'][i]['formattedUrl']
-						imdb_key = re.search('^www.*?title/(.*?)/.*',imdb_key)
-						print 'IMDB Key :: ', imdb_key.group(1)
-
-						
-						ik = imdb_key.group(1)
-						writer.writerow({'Entity':title,'Director':row['Director'],'Year':year,'Alias':alias,'url':url,'genre':genre,'Runtime':runtime,'IMDB URL':ik})
-						flag_1 = 1
-						break
-					else:
-						flag_2 = 1
-						pass
+					# json_year = res['items'][i]['pagemap']['metatags'][0]['title'].lower()
+					# json_year = re.sub(r'.*\(','',json_year)
+					# json_year = re.sub(r'\).*','',json_year).strip()
+					# print json_year.encode('utf-8')
+					
+					if re.sub(r'\s+\(.*','',json_title).strip() == t:
+						print "##"
+						if json_director is None:
+							print "###"
+							try:
+								print 'isnside try'
+								json_director = res['items'][i]['pagemap']['metatags'][0].get('og:description').lower()
+								print d
+								print json_director.encode('utf-8')
+								json_director = re.search(d,json_director,re.UNICODE)
+								if json_director:
+									print '####' 
+									if json_director.group(0) == d:
+										print '#####'
+										imdb_key = res['items'][i]['link']
+										print imdb_key
+										writer.writerow({'Entity':t,'Director':row['Director'],'Year':year,'Alias':alias,'url':url,'genre':genre,'Runtime':runtime,'IMDB_OLD': IMDB_OLD,'IMDB URL':imdb_key})
+										
+										check = 1
+										break
+									else:
+										print "######"
+								else:
+									print "director not found:"
+									# flag = 1		
+									
+							except:
+								print "Exception caught, no og:description"
+						# elif json_year == year:
+						# 	print "#######"
 							
-						#writer.writerow({'Title':row['Title'],'Director':row['Director'],'Key':''}) 
-	
-			else:
-			  	print "Imdb key not found"
-			if flag_1 == 0 and flag_2 == 1:
-				writer.writerow({'Entity':title,'Director':row['Director'],'Year':year,'Alias':alias,'url':url,'genre':genre,'Runtime':runtime,'IMDB URL':''})
+								
+					else:
+						print 'no key found'
+						flag = 1
+				if flag == 1 and check == 0:
+					print "VO == VO == VO"
+					print flag	
+					writer.writerow({'Entity':t,'Director':row['Director'],'Year':year,'Alias':alias,'url':url,'genre':genre,'Runtime':runtime,'IMDB_OLD': IMDB_OLD,'IMDB URL':imdb_key})
+			elif response is None:
+				print 'No Response'
+				writer.writerow({'Entity':t,'Director':row['Director'],'Year':year,'Alias':alias,'url':url,'genre':genre,'Runtime':runtime,'IMDB_OLD': IMDB_OLD,'IMDB URL':imdb_key})
+conn.close()
